94f376f6a662c66f3586825f55895faca231d1c1
Julia Wasilewska <julia.wasilewska@mini-siem.local>
2026-09-14 10:10:00 +0200

    SIEM-33 Panele alertów i statusu hosta
    Time spent: 4h
    Task: SIEM-33

--- grafana/queries.sql
+++ grafana/queries.sql
@@ -4,3 +4,8 @@ SELECT received_at, cpu FROM logs WHERE host='mini-siem-client1' ORDER BY id DES
 SELECT received_at, ram FROM logs WHERE host='mini-siem-client1' ORDER BY id DESC LIMIT 100;
 -- DISK
 SELECT received_at, disk FROM logs WHERE host='mini-siem-client1' ORDER BY id DESC LIMIT 100;
+
+-- Recent alerts
+SELECT * FROM alerts ORDER BY id DESC LIMIT 20;
+-- Total alerts
+SELECT COUNT(*) AS total_alerts FROM alerts;
